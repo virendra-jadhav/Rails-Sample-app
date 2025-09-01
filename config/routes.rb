2bd10e6108019a5d0103aaa52503b702b9d889a0
myusername <Virendra.Jadhav@dpind.com>
2025-09-01 19:24:26 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,9 @@ Rails.application.routes.draw do
   # get "/products", to: "products#index"
   root "products#index"
 
-  resources :products
+    # resources :products
+    resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+    resource :unsubscribe, only: [ :show ]
 end
